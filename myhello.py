6d9cfa6468be338e17303099ff0c390fc5b61579
王川 <wangchuan7@xiaomi.com>
2023-03-21 09:48:07 +0800

    first commit

---
+++ myhello.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/2/16 9:53
+# @Author  : wangchuan7
+# @FileName: myhello.py
+# @Software: PyCharm
+print ("relay.xiaomi.com")
+print ("login.mioffice.cn")
+print ("b2c.srv")
+print ("api.xiaomi.com")
+print ("youpin.mi.com")
+print ("api.mi.com")
+print ("proxy.srv")
+print ("test.xiaomi.proxy.srv")
+print ("test.xiaomi.srv")
+print ("11.xiaomi.srv")
+print ("112.xiaomi.srv")
+print ("112hello.xiaomi.srv")
---
+++ new.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/6 15:49
+# @Author  : wangchuan7
+# @FileName: new.py
+# @Software: PyCharm
+print ("relay.xiaomi.com")
+print ("login.mioffice.cn")
+print ("b2c.srv")
+print ("api.xiaomi.com")
+print ("youpin.mi.com")
+print ("api.mi.com")
+print ("proxy.srv")
+print ("test.xiaomi.proxy.srv")
+print ("test.xiaomi.srv")
+print ("11.xiaomi.srv")
+print ("112.xiaomi.srv")
+print ("112hello.xiaomi.srv")
